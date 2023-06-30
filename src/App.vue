388cff6bd48c2a986b6c9e7b677cb4bb1dfc7fd2
aslan-asilon3 <sulaslansetiawan3@gmail.com>
2023-06-29 20:02:51 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/welcome">Welcome</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/visitors/LandingPage.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    this is Landing Page
+  </main>
+</template>
